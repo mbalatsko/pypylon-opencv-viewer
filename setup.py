ce9196434985e56c71f538e48e661d0a9a6c8e6f
Maksym Balatsko <mbalatsko@gmail.com>
2018-09-28 22:57:43 +0200

    long description added

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ from setuptools import find_packages, setup
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='pypylon-opencv-viewer',
     packages=find_packages(),
     version='1.0',
     description='Easy to use Jupyter notebook interface connecting Basler Pylon images grabbing with openCV image processing.'
-    'Allows to specify interactive Jupyter widgets to manipulate Basler camera features values, grab camera image and at'
+    ' Allows to specify interactive Jupyter widgets to manipulate Basler camera features values, grab camera image and at'
     'once get an OpenCV window on which raw camera output is displayed or you can specify an image processing function,'
     'which takes on the input raw camera output image and display your own output.',
+	long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT License',
     author='Maksym Balatsko',
     author_email='mbalatsko@gmail.com',
